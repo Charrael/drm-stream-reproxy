21d314e21e4a310683f69b10f1495657a2c16449
Charrad Elias <elias.charrad@gmail.com>
2025-07-16 11:27:56 +0200

    progress on json database handling

--- src/include/data/DataHandling.cpp
+++ src/include/data/DataHandling.cpp
@@ -1,3 +1,41 @@
 #include <fstream>
 #include "nlohmann/json.hpp"
 
+using json = nlohmann::json;
+
+struct SelectedVariants{
+  std::string video;
+  std::vector<std::string> audio;
+};
+struct streamData
+{
+  std::string url;
+  bool active = true;
+
+  //metadata
+  std::string channelName;
+  std::string group;
+  std::string tvgID, tvgLogo;
+
+  //DRM
+  std::string videoDecryptionKey{};
+  std::string audioDecryptionKey{};
+
+  // Variant lists
+  std::vector<std::string> videoQualities;
+  std::vector<std::string> audioTracks;
+
+  // User‑selected variant
+  SelectedVariants selectedVariants;
+};
+
+struct channelConfig{
+  std::unordered_map<std::string, streamData> streams;
+};
+void loadJsonData(std::string *databaseLocation){
+  std::ifstream database(*databaseLocation);
+  json j_database = json::parse(database);
+
+  
+
+}
